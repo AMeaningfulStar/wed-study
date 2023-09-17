af119bc31c45fe391695ce5e81f0eb70ee4ef4d9
A Meaningful star <ghrlfehd123@naver.com>
2023-09-17 22:08:22 +0900

    프로젝트 재 생성

---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <Link href={'/project01'} >
+        하하
+      </Link>
+    </div>
+  )
+}
+
+{/* <div className="flex flex-col items-center justify-between w-[430px] gap-4">
+        <span className="font-bold text-2xl">
+          How do you want your project to be?
+        </span>
+        <div className="flex flex-row justify-start gap-4 w-36">
+          <div className="w-[80px] h-[40px] bg-gray-300 rounded-full p-[3px]">
+            <div className="w-[34px] h-[34px] rounded-full bg-white"></div>
+          </div>
+          <span className="leading-[40px]">Good</span>
+        </div>
+        <div className="flex flex-row justify-start gap-4 w-36">
+          <div className="w-[80px] h-[40px] bg-gray-300 rounded-full p-[3px]">
+            <div className="w-[34px] h-[34px] rounded-full bg-white"></div>
+          </div>
+          <span className="leading-[40px]">Cheap</span>
+        </div>
+        <div className="flex flex-row justify-start gap-4 w-36">
+          <div className="w-[80px] h-[40px] bg-gray-300 rounded-full p-[3px]">
+            <div className="w-[34px] h-[34px] rounded-full bg-white"></div>
+          </div>
+          <span className="leading-[40px]">Fast</span>
+        </div>
+      </div> */}
